aea316363f3e05cbe9f4cb76a3460ff1a2a06c1a
nmichelette <michel83@students.rowan.edu>
2018-10-12 14:56:49 -0400

    click

---
+++ Golfy Boi/Assets/Scripts/ClickingTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickingTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                
+                // the object identified by hit.transform was clicked
+                // do whatever you want
+            }
+        }
+    }
+
+}
