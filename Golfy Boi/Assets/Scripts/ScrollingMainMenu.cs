5e3d5c4e173ea8183d87d6649c16a3324b0997b1
Jaysson Solano <Jeroxoid@Outlook.com>
2018-11-01 00:43:07 -0400

    Added stuff for the mainmenu

---
+++ Golfy Boi/Assets/Scripts/ScrollingMainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingMainMenu : MonoBehaviour {
+    Vector2 offset;
+    Material mat;
+    public float XOffset, YOffset;
+	// Use this for initialization
+	void Start ()
+    {
+        offset = new Vector2(XOffset, YOffset);
+        mat = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mat.mainTextureOffset += offset * Time.deltaTime;
+	}
+}
