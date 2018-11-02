9c36a1866efdf8dc783ea999272c2fe589e707d9
CHicks256 <chicks-student@nfcs.k12.pa.us>
2018-11-02 15:29:58 -0400

    lksjd;pgfolakdsjfio;lkadshj;lfkjdsako]pf[;/aeds

--- Golfy Boi/Assets/Scripts/startButton.cs
+++ Golfy Boi/Assets/Scripts/startButton.cs
@@ -7,7 +7,7 @@ public class startButton : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+     
 	}
 	
 	// Update is called once per frame
@@ -16,19 +16,23 @@ public class startButton : MonoBehaviour {
 	}
     public void startGame()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("testScene");
     }
 
     public void endGame()
     {
-
+        Time.timeScale = 1;
+        Application.Quit();
     }
     public void mainMenu()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("MainMenu");
     }
     public void credits()
     {
+        Time.timeScale = 1;
 
     }
 }
