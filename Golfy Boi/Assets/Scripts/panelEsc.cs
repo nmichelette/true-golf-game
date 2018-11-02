e5189b9469d6a94b440f156198b4394f65c1e928
CHicks256 <chicks-student@nfcs.k12.pa.us>
2018-11-02 14:58:19 -0400

    Panel for game

---
+++ Golfy Boi/Assets/Scripts/panelEsc.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class panelEsc : MonoBehaviour {
+    public GameObject panel;
+ 
+	// Use this for initialization
+	void Start () {
+        panel.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            panel.gameObject.SetActive(true);
+        }
+	}
+}
