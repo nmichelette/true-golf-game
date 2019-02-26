62af96582d164529b1d7038e14e08e9591cb9976
Jeroxoid <Jeroxoid@outlook.com>
2019-02-25 22:58:30 -0500

    NEW DEATH MECHANIC
    doesn't work yet owo

---
+++ Golfy Boi/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour {
+
+    GameObject[] players;
+	// Use this for initialization
+	void Start ()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].GetComponent<Renderer>().isVisible == false)
+            {
+                Debug.Log("cant see" + players[i]);
+            }
+        }
+	}
+}
