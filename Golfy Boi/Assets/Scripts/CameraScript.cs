99b134cdb81796f5dc4da2a40ed38372a0734917
Jeroxoid <Jeroxoid@outlook.com>
2019-02-27 19:31:54 -0500

    New Camera, New Death Mechanics

--- Golfy Boi/Assets/Scripts/CameraScript.cs
+++ Golfy Boi/Assets/Scripts/CameraScript.cs
@@ -7,13 +7,29 @@ public class CameraScript : MonoBehaviour
     private Vector3 offset;
     private GameObject[] players;
     private int playerCount;
-    private float greatestX;
+    private int direction;//1=right, 2=left, 3=up, 4=down
+    public PlayerDeath death;
+    public int Direction
+    {
+        get
+        {
+            return direction;
+        }
+
+        set
+        {
+            direction = value;
+        }
+    }
+
     // Use this for initialization
     void Start()
     {
         offset = new Vector3(0.0f, 0.0f, -10.0f);
         players = GameObject.FindGameObjectsWithTag("Player");
         playerCount = players.Length;
+        death = GetComponent<PlayerDeath>();
+        direction = 1;
     }
 
     // Update is called once per frame
@@ -22,22 +38,64 @@ public class CameraScript : MonoBehaviour
         players = GameObject.FindGameObjectsWithTag("Player");
         if (target != null)
         {
-            transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, 0.25f);
-            for (int i = 0; i < players.Length; i++)
+            Vector3 yerd = new Vector3(0, 0, 0);
+            switch (direction)
             {
-                if (players[i].gameObject.transform.position.x > target.transform.position.x)
-                {
-                    greatestX = target.transform.position.x;
-                    target = players[i];
-                }
+                case 1://right
+
+                    yerd = new Vector3(target.transform.position.x, 0, 0);
+                    transform.position = Vector3.Lerp(transform.position, yerd + offset, 0.25f);
+                    for (int i = 0; i < players.Length; i++)
+                    {
+                        if (players[i].gameObject.transform.position.x > target.transform.position.x)
+                        {
+                            target = players[i];
+                        }
+                    }
+                    break;
+                case 2://left
+            
+                    yerd = new Vector3(target.transform.position.x, 0, 0);
+                    transform.position = Vector3.Lerp(transform.position, yerd + offset, 0.25f);
+                    for (int i = 0; i < players.Length; i++)
+                    {
+                        if (players[i].gameObject.transform.position.x < target.transform.position.x)
+                        {
+                            target = players[i];
+                        }
+                    }
+                    break;
+                case 3://up
+                    yerd = new Vector3(0, target.transform.position.y, 0);
+                    transform.position = Vector3.Lerp(transform.position, yerd + offset, 0.25f);
+                    for (int i = 0; i < players.Length; i++)
+                    {
+                        if (players[i].gameObject.transform.position.y > target.transform.position.y)
+                        {
+                            target = players[i];
+                        }
+                    }
+                    break;
+                case 4://down
+                    yerd = new Vector3(0, target.transform.position.y, 0);
+                    transform.position = Vector3.Lerp(transform.position, yerd + offset, 0.25f);
+                    for (int i = 0; i < players.Length; i++)
+                    {
+                        if (players[i].gameObject.transform.position.y < target.transform.position.y)
+                        {      
+                            target = players[i];
+                        }
+                    }
+                    break;
+                default:
+                    direction = 1;
+                    break;
             }
         }
         else
         {
-            Debug.Log("ungabunga");
             for (int i = 0; i < players.Length; i++)
             {
-                Debug.Log(players[i].ToString());
                 if (players[i].gameObject.transform.position.x > 0)
                 {
                     target = players[i];
--- Golfy Boi/Assets/Scripts/PlayerDeath.cs
+++ Golfy Boi/Assets/Scripts/PlayerDeath.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour {
 
-    public CameraScript script;
     public Camera cam;
     GameObject[] players;
-	// Use this for initialization
-	void Start ()
+    private int direction;//1 right, 2 left, 3 up, 4 down
+    public int Direction
+    {
+        get
+        {
+            return direction;
+        }
+
+        set
+        {
+            direction = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
     {
         players = GameObject.FindGameObjectsWithTag("Player");
-        script = GetComponent<CameraScript>();
-
+        direction = 1;
     }
 	
 	// Update is called once per frame
@@ -21,10 +33,34 @@ public class PlayerDeath : MonoBehaviour {
         players = GameObject.FindGameObjectsWithTag("Player");
         for (int i = 0; i < players.Length; i++)
         {
-            if (players[i].gameObject.transform.position.y < -10)
+            Vector3 curr = players[i].gameObject.transform.position;
+            Vector3 campos = cam.transform.position;
+            switch (direction)
             {
-                Debug.Log("cant see" + players[i]);
-                Destroy(players[i]);
+                case 1://right
+                    if ((curr.y < campos.y - cam.orthographicSize || curr.y < campos.y + cam.orthographicSize) || curr.x < campos.x - cam.orthographicSize * cam.aspect)
+                    {
+                        Destroy(players[i]);
+                    }
+                    break;
+                case 2://left
+                    if ((curr.y < campos.y - cam.orthographicSize || curr.y < campos.y + cam.orthographicSize) || curr.x > campos.x + cam.orthographicSize * cam.aspect)
+                    {
+                        Destroy(players[i]);
+                    }
+                    break;
+                case 3: //up
+                    if (curr.y < campos.y - cam.orthographicSize  || (curr.x < campos.x - cam.orthographicSize * cam.aspect || curr.x>campos.x +cam.orthographicSize*cam.aspect))
+                    {
+                        Destroy(players[i]);
+                    }
+                    break;
+                case 4://down
+                    if (curr.y > campos.y + cam.orthographicSize || (curr.x < campos.x - cam.orthographicSize * cam.aspect || curr.x > campos.x + cam.orthographicSize * cam.aspect))
+                    {
+                        Destroy(players[i]);
+                    }
+                    break;
             }
         }
     }
