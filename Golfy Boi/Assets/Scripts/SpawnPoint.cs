700bb4c4836536e130ad6b0b13278286abfcc0d5
Maffei <cmaffei97@gmail.com>
2019-03-04 15:37:24 -0500

    Spawner Prefab
    Spawner prefab that has values for player number and skin, MAKE SURE THE REFERENCE IMAGE IS AT 0,0,0

--- Golfy Boi/Assets/Scripts/SpawnPoint.cs
+++ Golfy Boi/Assets/Scripts/SpawnPoint.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnPoint : MonoBehaviour {
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject player;
+    public int playerNum;
+    public Sprite skin;
 
-    public GameObject s;
 	// Use this for initialization
-	void Start () {
-        Instantiate(s, this.transform);
+	void Start ()
+    {
+        print("Spawning Player "+playerNum+ " at "+ this.transform.position);
+        Instantiate(player);
+        player.transform.position = this.transform.position;
+        player.GetComponent<ControllerMovement>().playernum = playerNum;
+        player.GetComponent<SpriteRenderer>().sprite = skin;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
