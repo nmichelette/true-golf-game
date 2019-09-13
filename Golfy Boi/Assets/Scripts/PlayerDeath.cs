e7540a25f37c99d2066c9306728b35d184671341
Maffei <cmaffei97@gmail.com>
2019-09-13 12:27:44 -0400

    Revert "Ability & Death Timer"
    This reverts commit f979b1b5d850a3762622c8392e72f5ad84e1703b.

--- Golfy Boi/Assets/Scripts/PlayerDeath.cs
+++ Golfy Boi/Assets/Scripts/PlayerDeath.cs
@@ -34,11 +34,7 @@ public class PlayerDeath : MonoBehaviour {
         for (int i = 0; i < players.Length; i++)
         {
             Vector3 curr = players[i].gameObject.transform.position;
-            float[] playerTimer = new float[]{0f,0f,0f,0f};
-            bool[] trigger = new bool[] {true,true,true,true};
-            float DeathTimer = 2.0f;
             Vector3 campos = cam.transform.position;
-
 
             //Use to test if player is killed while in-bounds
             //print("Player x:"+ curr.x + "  Player y:" + curr.y);
@@ -51,17 +47,8 @@ public class PlayerDeath : MonoBehaviour {
                 case 1://right
                     if ((curr.y < campos.y - cam.orthographicSize || curr.y > campos.y + cam.orthographicSize) || curr.x < campos.x - cam.orthographicSize * cam.aspect)
                     {
-                        if (trigger[i])
-                        {
-                            playerTimer[i] = Time.time + DeathTimer;
-                            trigger[i] = false;
-                        }
-                        if(playerTimer[i]<=Time.time)
-                        {
-                            Destroy(players[i]);
-                            print("Killed Player " + (i + 1));
-                        }
-                        
+                        Destroy(players[i]);
+                        print("Killed Player "+ (i + 1));
                     }
                     break;
                 case 2://left
@@ -84,9 +71,6 @@ public class PlayerDeath : MonoBehaviour {
                         Destroy(players[i]);
                         print("Killed Player " + (i + 1));
                     }
-                    break;
-                default:
-                    trigger = new bool[] { true, true, true, true };
                     break;
             }
         }
